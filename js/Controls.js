c92b60d76056bba4d92bca0c9f07d0a8e79664e3
= <richmiddleditch@gmail.com>
2011-08-23 18:40:51 +0100

    disabling cursor key scroll

--- js/Controls.js
+++ js/Controls.js
@@ -7,7 +7,11 @@ define("Controls", ['Config', 'PubSub'], function(Config, PubSub) {
 			// TODO: add keyboard events directly to canvas? possible?
 			//var canvas = document.getElementById('asteroidCanvas');
 			//canvas.addEventListener('keydown', function(e) {
-			
+			document.onkeydown = function(e) {
+				if(e.keyCode >= 37 && e.keyCode <= 40) {
+					return false;
+				}
+			}
 			window.addEventListener('keydown', function(e) {
 				PubSub.publishSync('keydown', e.keyCode);
 			}, true);
